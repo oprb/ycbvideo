ec04779786432558fde99546bce534dd9fdedfb6
Ole Priebusch <opriebusch@web.de>
2021-11-19 18:47:53 +0100

    Add Simple Access to Frames

--- ycbvideo.py
+++ ycbvideo.py
@@ -18,6 +18,16 @@ class Frame(NamedTuple):
     label: ndarray
 
 
+class FrameSelector(NamedTuple):
+    frame_sequence_selection: str
+    frame_selection: str
+
+
+class FrameDescriptor(NamedTuple):
+    frame_sequence: str
+    frame: str
+
+
 class FrameSequence:
     def __init__(self, path: str):
         self._path = path
@@ -67,6 +77,7 @@ class YcbVideoLoader:
     def __init__(self, path: str, data_directory: str = 'data'):
         self._set_directory_path(path)
         self._data_directory = data_directory
+        self._available_data_frame_sequences = self.get_available_frame_sequences()
     """
         path: Path to the YCB-Video dataset root directory
     """
@@ -81,6 +92,26 @@ class YcbVideoLoader:
 
     def get_available_frame_sequences(self) -> List[str]:
         return os.listdir(f"{self._path}/{self._data_directory}")
+
+    def get_available_frames(self, frame_sequence: str) -> List[str]:
+        return [entry[:6] for entry in os.listdir(f"{self._path}/{self._data_directory}/{frame_sequence}") if entry.endswith('-color.png')]
+
+    @staticmethod
+    def get_frame_set_index(self, frame_set: Union[str, int]) -> str:
+        if not isinstance(frame_set, str) or not isinstance(frame_set, int):
+            raise TypeError("Index has to be of type 'str' or type 'int'")
+
+        index = frame_set
+        if isinstance(frame_set, str):
+            if match := re.match(r'00(?P<index>[0-9]{2})', frame_set):
+                index = int(match.group('index'))
+            else:
+                raise ValueError(f"Index has wrong format: {frame_set}")
+
+        if not 0 <= frame_set <= 91:
+            raise ValueError(f"Index has to be a value between 0 and 91: {frame_set}")
+
+        return "{:04d}".format(frame_set)
 
     def get_frame_sequence(self, index: Union[str, int]) -> FrameSequence:
         if isinstance(index, int):
@@ -114,3 +145,132 @@ class YcbVideoLoader:
             logging.warning(f"All frame sets are traversed but still indexes remaining: {indexes}")
 
         return frames
+
+    def get_frame(self, index: Union[str, Tuple[str, str]]) -> Frame:
+        if isinstance(index, str):
+            if match := re.match(r'^(?P<framesequence>[0-9]{4}):(?P<frame>[0-9]{6})$', index):
+                frame_sequence = match.group('framesequence')
+                frame = match.group('frame')
+
+                index = (frame_sequence, frame)
+            else:
+                raise ValueError(f"Invalid pattern (<frame_sequence>:<frame> required): {index}")
+
+        return self.get_frame_sequence(index[0]).get_frame(index[1])
+
+    @staticmethod
+    def _get_items(selection: str) -> List[str]:
+        list_pattern = re.compile(r'^\[(?P<items>[0-9]+(,[0-9]+)*)\]$')
+        single_item_pattern = re.compile(r'^(?P<item>[0-9]+|\*)$')
+
+        items = []
+        if match := list_pattern.match(selection):
+            items = [item for item in match.group('items').split(',')]
+        elif match := single_item_pattern.match(selection):
+            items.append(match.group('item'))
+        else:
+            raise ValueError(f"Descriptor syntax is incorrect: {selection}")
+
+        return items
+
+    @staticmethod
+    def _format_selection_item(item: str, kind_of_item: str) -> str:
+        if not (kind_of_item == 'frame_sequence' or kind_of_item == 'frame'):
+            raise ValueError(
+                f"Unknown kind of descriptor (has to be either 'frame_sequence' or 'frame'): {kind_of_item}")
+        if item == '*':
+            return item
+
+        return "{:04d}".format(int(item)) if kind_of_item == 'frame_sequence' else\
+            "{:06d}".format(int(item))
+
+    @staticmethod
+    def get_frame_selector(selection: str) -> FrameSelector:
+        selection_pattern = r'\[([0-9]{1,n}(,[0-9]{1,n})*)\]|([0-9]{1,n})|\*'
+        frameset_selection_pattern = selection_pattern.replace('n', '4')
+        frame_selection_pattern = selection_pattern.replace('n', '6')
+        combined_pattern = re.compile(
+            f"^(?P<frameset>{frame_selection_pattern}):(?P<frame>{frameset_selection_pattern})$")
+
+        if match := combined_pattern.match(selection):
+            frame_set_selection = match.group('frameset')
+            frame_selection = match.group('frame')
+
+            return FrameSelector(frame_set_selection, frame_selection)
+        else:
+            raise ValueError(f"Selection syntax is incorrect: '{selection}'")
+
+    @staticmethod
+    def get_frame_selectors(selections: List[str]) -> List[FrameSelector]:
+        frame_selectors = []
+
+        for index, selection in enumerate(selections):
+            try:
+                frame_selectors.append(YcbVideoLoader.get_frame_selector(selection))
+            except ValueError:
+                raise ValueError(
+                    f"Selection syntax is incorrect: Selection '{selection}' at index {index}") from ValueError
+
+        return frame_selectors
+
+    def get_frame_descriptors(self, frame_selector: FrameSelector) -> List[FrameDescriptor]:
+        frame_descriptors = []
+
+        frame_sequence_selection = YcbVideoLoader._get_items(frame_selector.frame_sequence_selection)
+        frame_selection = YcbVideoLoader._get_items(frame_selector.frame_selection)
+
+        actual_frame_sequence_selection = None
+        actual_frame_selection = None
+
+        if YcbVideoLoader._is_star_selection(frame_sequence_selection):
+            actual_frame_sequence_selection = sorted(self._available_data_frame_sequences)
+        else:
+            actual_frame_sequence_selection = [YcbVideoLoader._format_selection_item(frame_sequence, 'frame_sequence') for
+                                               frame_sequence in frame_sequence_selection]
+            for index, frame_sequence in enumerate(actual_frame_sequence_selection):
+                if frame_sequence not in self._available_data_frame_sequences:
+                    raise ValueError(
+                        f"""Data Frame Sequence is not available: {frame_sequence}
+                            (specified at index {index} in {frame_selector}""")
+
+        for frame_sequence in actual_frame_sequence_selection:
+            available_frames = sorted(self.get_available_frames(frame_sequence))
+
+            if YcbVideoLoader._is_star_selection(frame_selection):
+                frame_descriptors.extend([FrameDescriptor(frame_sequence, frame) for frame in available_frames])
+
+                continue
+            else:
+                actual_frame_selection = [YcbVideoLoader._format_selection_item(frame, 'frame') for
+                                          frame in frame_selection]
+
+            for index, frame in enumerate(actual_frame_selection):
+                if frame not in available_frames:
+                    raise ValueError(
+                        f"""Data Frame is not available: {frame_sequence}:{frame}
+                            (specified at index {index} in {frame_selector}""")
+
+                frame_descriptors.append(FrameDescriptor(frame_sequence, frame))
+
+        return frame_descriptors
+
+    def _get_frame(self, descriptor: FrameDescriptor) -> Frame:
+        return self.get_frame_sequence(descriptor.frame_sequence).get_frame(descriptor.frame)
+
+    @staticmethod
+    def _is_star_selection(selection: List[str]) -> bool:
+        return len(selection) == 1 and selection[0] == '*'
+
+    def frames(self, frames: List[str]) -> Iterable[Frame]:
+        frame_descriptors = self._get_descriptors_from_selections(frames)
+
+        for descriptor in frame_descriptors:
+            yield self._get_frame(descriptor)
+
+    def _get_descriptors_from_selections(self, selections: List[str]) -> List[FrameDescriptor]:
+        frame_descriptors = []
+
+        for selector in self.get_frame_selectors(selections):
+            frame_descriptors.extend(self.get_frame_descriptors(selector))
+
+        return frame_descriptors
