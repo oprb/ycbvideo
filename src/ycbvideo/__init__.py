4e1737b77dad15e37e0a09e24c25d73c9d9558aa
Ole Priebusch <opriebusch@web.de>
2022-01-15 17:17:59 +0100

    Import Types from frames() Method in __init__.py

--- src/ycbvideo/__init__.py
+++ src/ycbvideo/__init__.py
@@ -1,9 +1,13 @@
 import pkgutil
 
 from .loader import YcbVideoLoader
+from .datatypes import Frame, FrameDescriptor, Box
 
-__version__ = pkgutil.get_data(__package__, 'VERSION').decode('utf-8')
+__version__ = pkgutil.get_data(__package__, 'VERSION').decode('utf-8').rstrip()
 
 __all__ = [
-    'YcbVideoLoader'
+    'YcbVideoLoader',
+    'Frame',
+    'FrameDescriptor',
+    'Box'
 ]
